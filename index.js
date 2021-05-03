0ae84d1ddc69a76723c6ae7da613d5eddb29ccfa
Mohammad <alhaj.mhmd@gmail.com>
2021-05-04 01:56:25 +0800

    init the project - connect to mongodb -insert into db -

---
+++ index.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/Schooldb', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('connection success'))
+    .catch((err) => console.log(err));
+
+//1-this is student schema
+const studentschema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    RegDate: { type: Date, defualt: Date.now() },
+    IsActive: Boolean,
+    Phones: [String]
+});
+
+//2-model student (combile the student schema to student model)
+const Student = mongoose.model('Student', studentschema);
+
+async function createStudent() {
+    //3-create instance of the model student
+    const student = new Student({
+        name: 'Ahmad',
+        age: 33,
+        isActive: true,
+        phones: ['123', '123']
+    });
+
+    //4- insert the student in the db
+    const result = await student.save();
+    console.log(result);
+
+}
+createStudent();
+
+// now do: nodemon index to run the project
+// open mongodb compass to check the result
